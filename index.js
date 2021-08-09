f384afb9497f1073dcf7fa4f40a4b01372d16626
Theo Ferguson <ferguson.theo@gmail.com>
2021-08-09 19:07:14 -0400

    commit tree 4dde8eb5356dbf7f12a8c94829ff673a4f000c0e

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    return Math.abs(distance - 42);
+}
+
+function distanceFromHqInFeet(distance) {
+    return distanceFromHqInBlocks(distance) * 264;
+}
+
+function distanceTravelledInFeet(fromHere, toHere) {
+    return Math.abs((fromHere - toHere) * 264);
+}
+
+function calculatesFarePrice(fromHere, toHere) {
+    const distanceCalc = distanceTravelledInFeet(fromHere, toHere);
+    if (distanceCalc <= 400) {
+        return 0
+    } else if (distanceCalc > 400 && distanceCalc < 2000) {
+        return (distanceCalc - 400) * .02;
+    } else if (distanceCalc >= 2000 && distanceCalc < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
